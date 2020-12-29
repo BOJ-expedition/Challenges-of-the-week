540eaa89f2f3f8b31a4244a8faa2091733dea0a4
restover <76655411+restover@users.noreply.github.com>
2020-12-29 23:20:39 +0900

    add comment

--- 2020W53/jjin/pr_두개뽑아서더하기.js
+++ 2020W53/jjin/pr_두개뽑아서더하기.js
@@ -1,15 +1,13 @@
+//https://programmers.co.kr/learn/courses/30/lessons/68644
+
 function solution(numbers) {
     const args = numbers;
     let tmpArr = [];
 
     for(let i=0; i<args.length; i++){
         for(let j=0; j<args.length; j++){
-            if(i!=j){
-                tmpArr.push(args[i] + args[j]);
-            }
+            if(i!==j) tmpArr.push(args[i] + args[j]);
         }
     }
-
-    let answer = [...new Set(tmpArr)].sort((a,b)=>+a-+b);
-    return answer;
+    return [...new Set(tmpArr)].sort((a,b)=>+a-+b);
 }
