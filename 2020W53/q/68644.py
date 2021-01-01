8f327fd85cce153885643759452ad71df6420855
Mr. Question Mark <loremipsumquaestio@gmail.com>
2021-01-01 21:10:05 +0900

    remove assert

--- 2020W53/q/68644.py
+++ 2020W53/q/68644.py
@@ -4,6 +4,3 @@
 def solution(numbers):
   import itertools
   return sorted(set(map(sum, itertools.combinations(numbers, 2))))
-
-assert(solution([2,1,3,4,1]) == [2,3,4,5,6,7])
-assert(solution([5,0,2,7]) == [2,5,7,9,12])
