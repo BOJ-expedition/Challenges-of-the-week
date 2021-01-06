dc04dea31d73d5c2fbbc46f710d43f39c127abf4
restover <76655411+restover@users.noreply.github.com>
2021-01-07 01:44:38 +0900

    delete useless codes fix a variable name

--- 2021W01/jjin/pr_조이스틱.js
+++ 2021W01/jjin/pr_조이스틱.js
@@ -1,5 +1,5 @@
-// //https://programmers.co.kr/learn/courses/30/lessons/42860
-let name ="DAZAAAAAAAAZAZA"
+// https://programmers.co.kr/learn/courses/30/lessons/42860
+let name ="AABAAAAAAABBB"
 let answer = 0;
 
 let lastCharIdx = 0;
@@ -17,11 +17,11 @@ const getDY =(char)=>{
     let y;
     if(charCode < charCodeOfN) y = charCode-65;
     else                       y = 90-charCode+1;
+    console.log("y", y);
     return y;
 }
 
 for(let x=0; x<name.length; x++){
-    if(x<0) x = name.length+x;
     let char = name[x];
 
     answer += getDY(char);
@@ -30,11 +30,12 @@ for(let x=0; x<name.length; x++){
     for(let to=from+1; to<name.length; to++){
         const nextChar = name[to];
         if(nextChar && nextChar !== "A"){
-            const toLeft = name.length-lastCharIdx+from;
+            const toLeft = name.length-lastCharIdx+from+1;
             const toRight = to-from;
-            //console.log(from, to, toLeft < toRight)
-            const x = toLeft < toRight ? toLeft : toRight;
-            answer += x;
+
+            const dx = toLeft < toRight ? toLeft : toRight;
+            console.log("dx", dx)
+            answer += dx;
             break;
         }
         else{
