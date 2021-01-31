d056c1b6b46c4b75f66170e935d318c79ae14e03
Mr. Question Mark <loremipsumquaestio@gmail.com>
2021-01-31 23:29:48 +0900

    fix whitespace

--- 2021W03/q/42579.py
+++ 2021W03/q/42579.py
@@ -2,9 +2,9 @@
   Link: https://programmers.co.kr/learn/courses/30/lessons/42579
 """
 def solution(genres, plays):
-    m = dict((g, []) for g in genres)
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        m[g].append((i, p))
-    a = sorted(m.items(), key=lambda kv: -sum(p for i, p in kv[1]))
-    answer = list(i for g, v in a for i, p in sorted(v, key=lambda kv: -kv[1])[:2])
-    return answer
+  m = dict((g, []) for g in genres)
+  for i, (g, p) in enumerate(zip(genres, plays)):
+    m[g].append((i, p))
+  a = sorted(m.items(), key=lambda kv: -sum(p for i, p in kv[1]))
+  answer = list(i for g, v in a for i, p in sorted(v, key=lambda kv: -kv[1])[:2])
+  return answer
