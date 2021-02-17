50aea928ecff43cdd6de74e61ae0a12f61645a08
minnim6 <70092162+minnim6@users.noreply.github.com>
2021-02-17 10:19:56 +0900

    Create programmers_두정수사이의합.java

---
+++ 2021W07/JeomB/programmers_두정수사이의합.java
@@ -0,0 +1,15 @@
+class Solution {
+  public long solution(int a, int b) {
+        long answer = 0;
+        if (a <= b) {
+            for (int i = a; i <= b; i++) {
+                answer += i;
+            }
+        }else {
+            for (int i = a; a>=b; i--) {
+                answer += a--;
+            }
+        }
+        return answer;
+    }
+}
